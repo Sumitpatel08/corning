3a2e21712cfb1c7e94b6c362744b17aba5d9f96a
rohit3607 <rohitplayer87089@gmail.com>
2025-06-08 17:53:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,13 +25,19 @@ def get_indian_time():
 
 
 from aiohttp import web
-from .route import routes
+#from .route import routes
 
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("CodeXBotz")
 
 class Bot(Client):
     def __init__(self):
